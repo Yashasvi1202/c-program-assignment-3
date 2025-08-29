ef00fd2334a990bf6ef2557d362bf275da359671
Yashasvi1202 <vanani.yashu25@gmail.com>
2025-08-29 11:36:50 +0530

    n even numbers.c

---
+++ n even numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  int i,n;
+  printf("Enter the value of n");
+  scanf("%d",&n);
+  for(i=1;i<n+1;i++)
+  {
+      if(i%2==0){
+        printf("%d \n",i);
+      }
+  } return 0;
+}
